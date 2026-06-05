041f83af8f2f2b003332c9e92ef0bb5d4f1e78c9
Utkarsh Srivastava <utkarshsri3690@gmail.com>
2026-06-05 13:32:44 +0530

    ubdated screenshot

--- src/test/java/com/nopcommerce/stepdefinition/Hooks.java
+++ src/test/java/com/nopcommerce/stepdefinition/Hooks.java
@@ -1,10 +1,17 @@
 package com.nopcommerce.stepdefinition;
 
-import io.cucumber.java.Before;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import com.nopcommerce.base.base;
 
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 
 public class Hooks extends base {
 
@@ -15,7 +22,26 @@ public class Hooks extends base {
     }
 
     @After
-    public void afterScenario() {
+    public void afterScenario(Scenario scenario)
+            throws IOException {
+
+        if (!scenario.isFailed()) {
+
+            File src =
+                    ((TakesScreenshot) driver)
+                    .getScreenshotAs(OutputType.FILE);
+
+            File dest = new File(
+                    "src/test/resources/Screenshot/PASSED_"
+                    + scenario.getName().replace(" ", "_")
+                    + ".png");
+
+            FileUtils.copyFile(src, dest);
+
+            System.out.println(
+                    "Screenshot Saved : "
+                    + scenario.getName());
+        }
 
         tearDown();
     }
